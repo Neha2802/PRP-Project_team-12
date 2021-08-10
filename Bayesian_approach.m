c98d4067506fb44a53c5de63a06bc8c1035f7fe2
srinath987 <84823141+srinath987@users.noreply.github.com>
2021-08-10 23:45:10 +0530

    Update Bayesian_approach.m

--- Bayesian_approach.m
+++ Bayesian_approach.m
@@ -1,29 +1,35 @@
 clear
 close all
 
-    org_std = 8;
-    org_mu = 5;
 
-    r = org_mu + org_std.*randn(100,1);
+original_std = 8;
+orig_mu = 5;
+I = 10;
+r = orig_mu + original_std .* randn(I,1);
 
-    x_test = -20:0.01:30;
-    [alpha_post, beta_post, gamma_post, delta_post, x_prediction] = x_prediction(r, 1, 1, 1, 0, x_test);
+alpha=1;
+beta=1;
+gamma=1;
+delta=0;
+
+x_test = -20:0.01:30;
+[alpha_post, beta_post, gamma_post, delta_post, x_prediction] = x_prediction(r, alpha, beta, 1, 0, x_test);
 
 
-    [map_mu, map_var] = map_norm(r, 1, 1, 1, 0);
-    map_sig = sqrt(map_var);
+[map_mu, map_var] = map_norm(r, alpha, beta, gamma, delta);
+map_sig = sqrt(map_var);
 
-    original=zeros(1,length(x_test));
-    map = zeros(1,length(x_test));
-    for nn=1:length(x_test)
-        original (nn)= caluclate_pdf(x_test(nn), org_mu, org_std);
-        map(nn) = caluclate_pdf(x_test(nn), map_mu, map_sig);
-    end
+original=zeros(1,length(x_test));
+map = zeros(1,length(x_test));
+for nn=1:length(x_test)
+original (nn)= caluclate_pdf(x_test(nn), orig_mu, original_std);
+map(nn) = caluclate_pdf(x_test(nn), map_mu, map_sig);
+end
 
 %plot(x_test, original, 'g', x_test, map, 'b', x_test, x_prediction, 'r');
-    semilogy(x_test, original, 'g', x_test, map, 'b', x_test, x_prediction, 'r');
-    xlabel('x');
-    ylabel('log (P(x))');
-    legend('Original', 'MAP', 'Bayesian');
-    axis([-20,30,0,0.1]);
-    set(gca,'XTick',[],'YTick',[]);
+semilogy(x_test, original, 'g', x_test, map, 'b', x_test, x_prediction, 'r');
+xlabel('x');
+ylabel('log (P(x))');
+legend('Original', 'MAP', 'Bayesian');
+axis([-20,30,0,0.1]);
+set(gca,'XTick',[],'YTick',[]);
